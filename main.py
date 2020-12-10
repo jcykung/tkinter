37d57f856313a7dfba2f2844a82240dff76914aa
Jonathan Kung <jkung@deltasd.bc.ca>
2020-12-10 14:58:00 -0800

    yup

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ window.title("Welcome to Me")
 
 window.geometry('350x200')
 
+
 def clicked():
     messagebox.showinfo('Message title', 'Message content')
+
 
 btn = Button(window, text='Click here', command=clicked)
 
