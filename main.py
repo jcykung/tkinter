716e1de3a6551203f66572db63edeb429cb35198
coolcat <Jonathan@Kung-iMac.local>
2020-12-09 21:31:03 -0800

    commit

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-import tkinter
+from tkinter import *
+from tkinter import messagebox
 
-top = tkinter.Tk()
+window = Tk()
 
-top.mainloop()
+window.title("Welcome to Me")
+
+window.geometry('350x200')
+
+def clicked():
+    messagebox.showinfo('Message title', 'Message content')
+
+btn = Button(window, text='Click here', command=clicked)
+
+btn.grid(column=0, row=0)
+
+window.mainloop()
